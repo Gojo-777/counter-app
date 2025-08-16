6c455596c77d5362302e5c4a9b8b0cb54514d46e
Vivek Deshmukh <vivekdeshmukh997@gmail.com>
2025-08-16 16:37:55 +0530

    Update counter app with Tailwind config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[count,changed]= useState(0);
+  function decrementHandler(){
+    changed(count-1);
+  } 
+  function incrementHandler(){
+    changed(count+1);
+  }
+  function resetHandler(){
+    changed(0);
+  }
+
   return (
-    <div className="App">
-      
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
+      <div className='text-[#0398d4] font-medium text-2xl'>
+        Increment & Decrement
+      </div>
+      <div className='flex flex-row bg-white justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]'>
+        <button onClick={decrementHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+          - 
+        </button>
+        <div className='font-bold gap-12 text-5xl'>
+          {count}
+        </div>
+        <button onClick={incrementHandler} className='border-l-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg'>Reset</button>
     </div>
   );
 }
